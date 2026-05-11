f7ade941f5cdae7a9cb943ed417aefe48d642def
vania Ribeiro <vania_ribeiro2@outlook.com>
2026-05-11 15:23:22 -0300

    adicionei um projeto

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = document.querySelectorAll('.list .item')
+
+let indicators = document.querySelector('.indicator')
+let dots = document.querySelector(' ul li.active') 
+let list = document.querySelector('.list')
+
+
+
+
+let active = 0
+let firstPosition = 0
+let lastPosition = items.length - 1
+
+
+
+nextButton.onclick = () => {
+ 
+   
+let itemOld = document.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+   
+
+
+list.style.setProperty('--calculation',  1) 
+    active= active + 1 > lastPosition ? 0 : active + 1
+    items[active].classList.add('active')
+
+   
+
+
+
+  
+}
+prevButton.onclick = () => {
+  list.style.setProperty('--calculation',  - 1)  
+let itemNovo = document.querySelector('.list .item.active')
+ itemNovo.classList.remove('active')
+ active = active - 1 < firstPosition ? lastPosition : active - 1
+ items[active].classList.add('active')
+
+    
+   
+
+}
+
